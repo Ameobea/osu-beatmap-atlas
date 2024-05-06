277be429aed58aab57077f18019f2b0f0b30757b
Casey Primozic <me@ameo.link>
2024-05-06 14:56:25 -0700

    Enable source maps in production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { optimizeCss } from 'carbon-preprocess-svelte';
 import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-  plugins: [sveltekit(), glsl()],
+  plugins: [sveltekit(), glsl(), optimizeCss()],
   optimizeDeps: {
     exclude: ['carbon-components-svelte', 'carbon-icons-svelte', 'carbon-pictograms-svelte'],
   },
+  build: {
+    sourcemap: true,
+  }
 });
