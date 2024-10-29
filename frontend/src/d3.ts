00aed5efc0f52a6eacafa8ca790b0d5d879f03c3
Casey Primozic <me@ameo.link>
2024-10-28 18:49:12 -0700

    Remove some unused imports

--- frontend/src/d3.ts
+++ frontend/src/d3.ts
@@ -1,4 +1,3 @@
 export { axisBottom } from 'd3-axis';
-export { interpolate, interpolateRound, quantize } from 'd3-interpolate';
 export { scaleLinear } from 'd3-scale';
-export { create, select } from 'd3-selection';
+export { select } from 'd3-selection';
